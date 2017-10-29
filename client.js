fdc7cd30c0510591adfb25df0ecba135735d333d
ralphchristianeclipse <ralphchristianeclipse@gmail.com>
2017-10-28 23:04:36 -0700

    moveMotor

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 const five = require("johnny-five");
-const socket = require("socket.io-client")("http://localhost");
-// const board = new five.Board()
-
-// board.on("ready")
+const socket = require("socket.io-client")("https://f54464bd.ngrok.io");
 
 socket.on("connect", () => console.log("HELLO"));
 
-socket.on("board", data => {
-  console.log(data);
-});
+socket.on("board", data => {});
 socket.on("ui", data => {
-  console.log(data);
+  socket.emit("board", data);
+  moveMotor(data);
 });
+
+const moveMotor = data => {
+  console.log(data);
+};
