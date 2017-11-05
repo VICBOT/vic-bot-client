438bb6e0b883c2c041d08fbd55d81b6da78778cc
ralphchristianeclipse <ralphchristianeclipse@gmail.com>
2017-11-05 08:43:18 +0000

    Working Motors

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 const Raspi = require("raspi-io");
 const five = require("johnny-five");
 const { HOST, PORT } = require("./config.json");
-const socket = require("socket.io-client")(`${HOST}:${PORT}`);
+const socket = require("socket.io-client")(`http://${HOST}:${PORT}`);
+//const say = require("say")
+
 const board = new five.Board({
-  io: new Raspi(),
-  repl: true
+    io: new Raspi(),
+    repl: false
+});
+let motors
+board.on("ready", async() => {
+    console.log("YEah");
+    //await say.speak(`Board Ready`)
+    motors = new five.Motors([{
+        pins: {
+            pwm: "GPIO12",
+            dir: "GPIO6"
+        },
+        pins: {
+            pwm: "GPIO13",
+            dir: "GPIO19"
+        }
+    }]);
+    motors.forEach(motor => {
+        motor.on("start", () => {
+            console.log("Started")
+        })
+    })
+
+    motors.forward(255)
 });
 
-board.on("ready", () => {
-  console.log("YEah");
+board.on("info", console.log)
+board.on("ready", console.log)
+board.on("warn", console.log)
+board.on("fail", console.log)
 
-  const motor = new five.Motor({
-    pins: {
-      pwm: "GPIO12",
-      dir: "GPIO6"
-    }
-  });
-});
-socket.on("connect", () => console.log("HELLO"));
+socket.on("connect", () => console.log(`Socket connected to ${HOST}:${PORT}`));
 
 socket.on("board", data => {});
 socket.on("ui", data => {
-  socket.emit("board", data);
+
+    socket.emit("board", data);
+    if (data.motor && typeof data.motor !== "string")
+        moveMotor(data.motor)
 });
+
+const moveMotor = ({ direction: { x, y, angle }, speed }) => {
+    console.log(x, y, angle, speed)
+    if (angle === "up")
+        motors.forward(speed)
+    else
+        motors.brake()
+}
---
+++ index.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as gpio
+import time
+
+
+def init():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(12, gpio.OUT)
+    gpio.setup(6, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(19, gpio.OUT)
+
+def forward(tf):
+    gpio.output(12, True)
+    gpio.output(6, False)
+    time.sleep(tf)
+
+def reverse(tf):
+    gpio.output(12, False)
+    gpio.output(6, True)
+    time.sleep(tf)
+
+
+init()
+forward(2)
+reverse(2)
+
+gpio.cleanup()
+		
