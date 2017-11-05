4aa7f7e7a4e4584b366498e397c3fdaf979471a4
ralphchristianeclipse <ralphchristianeclipse@gmail.com>
2017-11-05 03:54:23 +0000

    New

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const Raspi = require("raspi-io")
 const five = require("johnny-five");
 const socket = require("socket.io-client")("https://f54464bd.ngrok.io");
+const board = new five.Board({
+  io: new Raspi()
+})
 
+board.on("ready",() => {
+  console.log("YEah")
+})
 socket.on("connect", () => console.log("HELLO"));
 
 socket.on("board", data => {});
 socket.on("ui", data => {
   socket.emit("board", data);
-  moveMotor(data);
 });
 
-const moveMotor = data => {
-  console.log(data);
-};
+
