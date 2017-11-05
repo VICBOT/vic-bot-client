721790d42a7ee033264a3ead3b67afebe725f356
ralphchristianeclipse <ralphchristianeclipse@gmail.com>
2017-11-05 06:12:15 +0000

    motor

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const Raspi = require("raspi-io")
+const Raspi = require("raspi-io");
 const five = require("johnny-five");
-const socket = require("socket.io-client")("https://f54464bd.ngrok.io");
+const { HOST, PORT } = require("./config.json");
+const socket = require("socket.io-client")(`${HOST}:${PORT}`);
 const board = new five.Board({
-  io: new Raspi()
-})
+  io: new Raspi(),
+  repl: true
+});
 
-board.on("ready",() => {
-  console.log("YEah")
-})
+board.on("ready", () => {
+  console.log("YEah");
+
+  const motor = new five.Motor({
+    pins: {
+      pwm: "GPIO12",
+      dir: "GPIO6"
+    }
+  });
+});
 socket.on("connect", () => console.log("HELLO"));
 
 socket.on("board", data => {});
 socket.on("ui", data => {
   socket.emit("board", data);
 });
-
-
